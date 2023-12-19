eef415776836eb06ddbf32cfd9948d7a07e11bea
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2023-12-19 19:18:58 +0800

    feat: Implement camera free-look on bed

--- world/bed.gd
+++ world/bed.gd
@@ -3,6 +3,9 @@ extends StaticBody3D
 @onready var _player = Global.player
 @onready var _camera = $Camera
 
+var _camera_yaw = 0.0
+var _camera_pitch = 0.0
+
 func _ready():
 	assert(_player != null)
 
@@ -11,6 +14,20 @@ func _input(event):
 		await get_tree().create_timer(0.1).timeout
 		_camera.clear_current()
 		Global.player = _player
+	if event is InputEventMouseMotion and _camera.is_current():
+		var sensitivity = Global.camera_default_sensitivity
+		var mouse_position = event.relative * sensitivity
+		var yaw = mouse_position.x
+		var pitch = mouse_position.y
+		
+		mouse_position = Vector2.ZERO
+		yaw = clamp(yaw, -40 - _camera_yaw, 60 - _camera_yaw)
+		_camera_yaw += yaw
+		pitch = clamp(pitch, -40 - _camera_pitch, 60 - _camera_pitch)
+		_camera_pitch += pitch
+		
+		_camera.rotate_y(deg_to_rad(-yaw))
+		_camera.rotate_object_local(Vector3(1, 0, 0), deg_to_rad(-pitch))
 
 func _process(_delta):
 	if _camera.current:
