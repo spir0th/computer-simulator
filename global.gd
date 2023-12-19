a04db39cf6449993a5249fe0b6e941c0dbb54cc2
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2023-12-19 23:39:36 +0800

    feat: Changes to global.gd

--- global.gd
+++ global.gd
@@ -1,6 +1,5 @@
 extends Node
 
-#region Variables
 var application_name = ProjectSettings.get_setting("application/config/name", "Godot Engine")
 var application_version = ProjectSettings.get_setting("application/config/version", Engine.get_version_info().string)
 
@@ -9,8 +8,6 @@ var camera_default_fov = 75.0 # Default camera FOV (field-of-view)
 
 var player # The player node that is currently being used in-game.
 var menu # The menu node, it may be null or non-null, depending on the current scene.
-#endif
-#region Startup
+
 func _init():
 	process_mode = Node.PROCESS_MODE_ALWAYS
-#endregion
