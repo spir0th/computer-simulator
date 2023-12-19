4317b47f1587a22ba33bd42dd36bed8b871d64e8
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2023-12-19 23:35:31 +0800

    refactor: Cleanup on global.gd

--- global.gd
+++ global.gd
@@ -1,18 +1,16 @@
 extends Node
 
+#region Variables
+var application_name = ProjectSettings.get_setting("application/config/name", "Godot Engine")
+var application_version = ProjectSettings.get_setting("application/config/version", Engine.get_version_info().string)
+
+var camera_default_sensitivity = 0.25 # Default camera sensitivity
+var camera_default_fov = 75.0 # Default camera FOV (field-of-view)
+
+var player # The player node that is currently being used in-game.
+var menu # The menu node, it may be null or non-null, depending on the current scene.
+#endif
 #region Startup
 func _init():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 #endregion
-#region Application info
-var application_name = ProjectSettings.get_setting("application/config/name", "Godot Engine")
-var application_version = ProjectSettings.get_setting("application/config/version", Engine.get_version_info().string)
-#endregion
-#region Camera defaults
-var camera_default_sensitivity = 0.25
-var camera_default_fov = 75.0
-#endregion
-#region Variables
-var player # The player node that is currently being used in-game.
-var menu # The menu node, it may be null or non-null, depending on the current scene.
-#endif
