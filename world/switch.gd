5dd127fd5e5754ae79ef449fd24f95237a092002
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2023-12-22 23:42:10 +0800

    Fix volume inconsistency on some AudioStreamPlayer3Ds

--- world/switch.gd
+++ world/switch.gd
@@ -3,16 +3,16 @@ extends "res://world/interactable.gd"
 @export var _sun: DirectionalLight3D
 @export var _light: OmniLight3D
 
-@onready var _switcher_animator = $Switcher/Animator
+@onready var _animator = $Animator
 
 func _on_interacted():
 	if _light == null:
 		return
 	if _light.omni_range < 1:
-		_switcher_animator.play("on")
+		_animator.play("on")
 		_sun.light_energy = 0.0
 		_light.omni_range = 10.0
 	else:
-		_switcher_animator.play("off")
+		_animator.play("off")
 		_sun.light_energy = 1.0
 		_light.omni_range = 0.0
