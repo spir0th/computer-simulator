11f7315c0f77743d5026038fe1562fff5a202108
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2023-12-24 18:45:46 +0800

    Refactor some lines in settings.gd

--- settings.gd
+++ settings.gd
@@ -21,13 +21,11 @@ func restore_defaults():
 func _init_custom_properties():
 	# Read values from custom properties, since ProjectSettings does not handle them by default.
 	Global.application_name = ProjectSettings.get_setting("application/config/name", "Godot Engine")
-	Global.application_version = ProjectSettings.get_setting("application/config/version", Engine.get_version_info().string)
+	Global.application_version = ProjectSettings.get_setting("application/config/version", Engine.get_version_info())
 	
 	Global.camera_default_sensitivity = ProjectSettings.get_setting("input_devices/pointing/camera/sensitivity", 0.25)
 	Global.camera_default_smoothness = ProjectSettings.get_setting("input_devices/pointing/camera/smoothness", 30)
 	Global.camera_default_fov = ProjectSettings.get_setting("rendering/camera/field_of_view/value", 75.0)
 	
-	var master_vol = ProjectSettings.get_setting("audio/buses/volume/master", -4.8)
-	var output_device = ProjectSettings.get_setting("audio/devices/output_device", "Default")
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), master_vol)
-	AudioServer.output_device = output_device
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), ProjectSettings.get_setting("audio/buses/volume/master", -4.8))
+	AudioServer.output_device = ProjectSettings.get_setting("audio/devices/output_device", "Default")
