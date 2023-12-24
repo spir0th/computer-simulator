74ac903f17d4287502eb470a7dd8520a89572102
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2023-12-24 18:58:18 +0800

    Fix clicking the play button twice causes game to crash (#10)

--- main.gd
+++ main.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
+var _menu_started = false
+
 func _ready():
 	# Make sure the game is not frozen.
 	get_tree().paused = false
 
 func _on_menu_started():
+	if _menu_started:
+		return
+	
 	get_tree().change_scene_to_file("res://game.tscn")
+	_menu_started = true
